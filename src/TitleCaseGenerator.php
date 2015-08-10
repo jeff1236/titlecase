b51e16deac7ab12c6eb8e080e9ce1b9b4e87485c
Chris Lytsell & Jeff Franco <student@epicodus.com>
2015-08-10 12:31:34 -0700

    start code for spec 4

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -4,7 +4,19 @@
     function makeTitleCase($input_title)
     {
       $cap_title = ucwords($input_title, " ");
+      $non_caps = array["a", "an", "the", "to", "at", "in", "with", "and", "but", "for", "or", "nor"];
+        foreach($cap_title as $word) {
+          foreach($non_caps as $article) {
+            if($word == $article) {
+              
+            }
+          }
+
+        }
+
       return $cap_title;
+
+
     }
   }
 
